f3727064b249e02bd594fee7f2c6070f33fa0cb1
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-01-12 16:59:00 -0800

    Modifies names of form elements to correctly pass the parameters to #create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    #@user = User.new(user_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
-    #if @user.save
-    #  redirect_to root
-    #else
-    #  render :new, status: :unprocessable_entity
-    #end
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
