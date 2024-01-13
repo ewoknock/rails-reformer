3922b0d8be275c57fa90926a8d1cc725e3283c05
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-01-12 16:47:33 -0800

    Modifies form and controller to correctly respond with a 204

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    #@user = User.new(user_params)
 
-    if @user.save
-      redirect_to @user
-    else
-      render :new, status: :unprocessable_entity
-    end
+    #if @user.save
+    #  redirect_to root
+    #else
+    #  render :new, status: :unprocessable_entity
+    #end
   end
 
   private
